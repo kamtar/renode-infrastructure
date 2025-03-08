0419a3fc7c3c01dce43231eb35c81fc88511dcbe
Tomáš Polišenský <polisensky@cutter.cz>
2025-03-08 16:12:55 +0100

    minor changes

--- src/Emulator/Main/Testing/RawUartTester.cs
+++ src/Emulator/Main/Testing/RawUartTester.cs
@@ -10,6 +10,8 @@ using System.Runtime.CompilerServices;
 using Antmicro.Renode.Peripherals.Cutter.SPIDevices;
 using Antmicro.Renode.Peripherals.UART;
 using IronPython.Runtime;
+using Antmicro.Renode.Logging;
+using Antmicro.Renode.Exceptions;
 
 namespace Antmicro.Renode.Testing
 {
@@ -33,12 +35,16 @@ namespace Antmicro.Renode.Testing
         public RawUartTester(IUART uart)
         {
             uart.TryGetMachine(out machine);
+            if(machine == null)
+            {
+                throw new ArgumentException("Could not find machine for UART");
+            }
             this.uart = uart;
             uart.CharReceived += (b) => readBytes.Add(b);
         }
 
         public void WriteBytes(byte[] bytes)
-        {
+        {  
             foreach(var b in bytes)
             {
                 uart.WriteChar(b);
@@ -49,7 +55,7 @@ namespace Antmicro.Renode.Testing
         {
             var startTime = machine.ElapsedVirtualTime.TimeElapsed.TotalMilliseconds;
             
-            uart.CharReceived += (b) => startTime = machine.ElapsedVirtualTime.TimeElapsed.TotalMilliseconds;
+            //uart.CharReceived += (b) => startTime = machine.ElapsedVirtualTime.TimeElapsed.TotalMilliseconds;
           
             while(machine.ElapsedVirtualTime.TimeElapsed.TotalMilliseconds - startTime < timeoutMs)
             {
@@ -60,12 +66,20 @@ namespace Antmicro.Renode.Testing
             return result;
         }
 
-        public byte[] ReadNumBytes(uint numBytes, uint timeoutMs=10000)
+        public byte[] ReadNumBytes(uint numBytes, uint timeoutMs=1000)
         {
             var startTime = machine.ElapsedVirtualTime.TimeElapsed.TotalMilliseconds;
-          
-            while(machine.ElapsedVirtualTime.TimeElapsed.TotalMilliseconds - startTime < timeoutMs)
+
+            var masterTimeSource = EmulationManager.Instance.CurrentEmulation.MasterTimeSource;
+            var timeoutEvent = masterTimeSource.EnqueueTimeoutEvent((uint)(30 * 1000));
+
+            while((machine.ElapsedVirtualTime.TimeElapsed.TotalMilliseconds - startTime) < timeoutMs)
             {
+                if(timeoutEvent.IsTriggered)
+                {
+                    throw new TimeoutException("ReadNumBytes Realtime timeout!");
+                }
+                
                 if(readBytes.Count >= numBytes)
                 {
                     break;
